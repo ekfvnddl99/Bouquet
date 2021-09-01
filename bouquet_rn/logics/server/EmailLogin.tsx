82046722d4b565d5f4a9982c503ef1f0cde5e634
Go Gwangseo <rufflain918@gmail.com>
2021-09-01 15:02:31 +0900

    Modify: Sync APIs to Swagger

--- bouquet_rn/logics/server/EmailLogin.tsx
+++ bouquet_rn/logics/server/EmailLogin.tsx
@@ -1,42 +1,54 @@
-import {Base64} from 'js-base64';
 import * as APIs from './APIUtils';
 
-
-
-export async function loginEmailAsync(email: string, password: string): APIs.ResultOutput {
+export async function loginEmailAsync(
+  email: string,
+  password: string,
+): APIs.ResultOutput {
   const result = await APIs.postAsync(
-    "/auth/login/Email",
-    { 'email': email, 'pw': password, 'Content-Type': 'application/json' },
+    '/auth/login/Email',
+    { 'Content-Type': 'application/json' },
     JSON.stringify({
-      "email": email,
-      "pw": password
+      email,
+      pw: password,
     }),
-    false
+    false,
   );
-  
-  if (typeof(result) !== "string") return result.Authorization;
+
+  if (typeof result !== 'string') return result.Authorization;
   return result;
 }
 
-export async function registerEmailAsync(email: string, password: string, name: string, profilePic: string): APIs.ResultOutput {
+export async function registerEmailAsync(
+  email: string,
+  password: string,
+  name: string,
+  profilePic: string,
+): APIs.ResultOutput {
   const result = await APIs.postAsync(
-    "/auth/register/Email",
-    { 'email': email, 'pw': password, 'Content-Type': 'application/json' },
+    '/auth/register/email',
+    { 'Content-Type': 'application/json' },
     JSON.stringify({
-      "email": email,
-      "pw": password,
-      "profile_img": profilePic,
-      "name": Base64.encode(name)
+      email,
+      pw: password,
+      profile_img: profilePic,
+      name,
     }),
-    false
+    false,
   );
-  
-  if (typeof(result) !== "string") return result.Authorization;
+
+  if (typeof result !== 'string') return result.Authorization;
   return result;
 }
 
 export async function checkEmailAsync(email: string): APIs.ResultOutput {
-  const result = await APIs.getAsync("/auth/dup_email", { 'email': email });
-  if (typeof(result) !== "string") return result.result;
+  const result = await APIs.postAsync(
+    '/auth/email/check',
+    { 'Content-Type': 'application/json' },
+    JSON.stringify({
+      email,
+    }),
+    false,
+  );
+  if (typeof result !== 'string') return result.duplicated;
   return result;
 }
--- bouquet_rn/logics/server/User.tsx
+++ bouquet_rn/logics/server/User.tsx
@@ -1,13 +1,27 @@
 import * as APIs from './APIUtils';
 
-// PUT
-export async function editUserAsync(name: string, profile: string): APIs.ResultOutput {
-  const result = APIs.putAsync("/user/me", JSON.stringify({"profile_img" : profile, "name":name}), true);
+// GET
+export async function getUserAsync(): APIs.ResultOutput {
+  const result = await APIs.getAsync('/user/', true);
+  return result;
+}
+
+// PATCH
+export async function editUserAsync(
+  name: string,
+  profile: string,
+): APIs.ResultOutput {
+  const result = await APIs.patchAsync(
+    '/user/',
+    true,
+    undefined,
+    JSON.stringify({ profile_img: profile, name }),
+  );
   return result;
 }
 
 // DELETE
 export async function deleteUserAsync(): APIs.ResultOutput {
-  const result = APIs.deleteAsync("user/me", true);
+  const result = await APIs.deleteAsync('user/', true);
   return result;
 }
