f8f3fef46bd494289d7a7d164e98fdf4b839a3df
Go Gwangseo <rufflain918@gmail.com>
2021-10-28 15:10:22 +0900

    Fix: OpenGraph image path

--- bouquet_landing/pages/index.tsx
+++ bouquet_landing/pages/index.tsx
@@ -22,7 +22,7 @@ export default function Home() {
         <meta property="og:type" content="website" />
         <meta property="og:title" content="Bouquet : 부캐들의 이야기 공간" />
         <meta property="og:description" content="Bouquet는 여러 부캐를 만들어 활동할 수 있는 SNS입니다. 자신의 여러 모습을 자유롭게 드러내 보세요." />
-        <meta property="og:image" content="../public/OpenGraph.png" />
+        <meta property="og:image" content="/OpenGraph.png" />
         <meta property="og:url" content="https://bouquet.ooo" />
         <meta property="og:site_name" content="Bouquet" />
         <meta property="og:image:width" content="1200" />
@@ -31,7 +31,7 @@ export default function Home() {
         <meta name="twitter:card" content="summary" />
         <meta name="twitter:title" content="Bouquet : 부캐들의 이야기 공간" />
         <meta name="twitter:description" content="Bouquet는 여러 부캐를 만들어 활동할 수 있는 SNS입니다. 자신의 여러 모습을 자유롭게 드러내 보세요." />
-        <meta name="twitter:image" content="../public/OpenGraph.png" />
+        <meta name="twitter:image" content="/OpenGraph.png" />
 
         <meta property="al:ios:url" content="https://apps.apple.com/kr/app/bouquet-%EB%B6%80%EC%BA%90%EB%93%A4%EC%9D%98-%EC%9D%B4%EC%95%BC%EA%B8%B0-%EA%B3%B5%EA%B0%84/id1589432470" />
         <meta property="al:ios:app_store_id" content="1589432470" />
